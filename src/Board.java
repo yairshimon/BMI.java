6b036d5839e8f3201ed50e670b6a228f9c52877d
yair shimon <shai@gmail.com>
2022-05-20 16:52:25 +0300

    4

--- src/Board.java
+++ src/Board.java
@@ -4,9 +4,13 @@ import java.awt.event.*;
 
 public class Board extends JPanel {
     private final ImageIcon imageIcon;
-    private UserDetails s2 ;
+    private UserDetails s2;
     private SlideColorPanel s3;
     private UserDetails s4;
+    private JButton clear, send;
+    private JLabel label;
+    private JPanel buttonPanel;
+
     public Board(int x, int y, int width, int height) {
         this.imageIcon = new ImageIcon("1.jpg");
         this.setBounds(x, y, width, height);
@@ -20,66 +24,81 @@ public class Board extends JPanel {
         s2.Gender();
         s2.BodyFrame();
         s4.detailsUser();
-        JLabel header1 = new JLabel ("BMI test");
-        JLabel header2 = new JLabel ("Please complete this form. Press the Submit button when complete ");
+        JLabel header1 = new JLabel("BMI test");
+        JLabel header2 = new JLabel("Please complete this form. Press the Submit button when complete ");
         JPanel header = new JPanel(true);
         header.add(header1);
         header.add(new JLabel("               "));
         header.add(header2);
-        header.setPreferredSize (new Dimension(200, 50));
-        add (header,BorderLayout.NORTH);
-        add (s2, BorderLayout.CENTER);
-        add (s3, BorderLayout.EAST);
-        add (s4, BorderLayout.WEST);
-
-       // repaint();
-        JButton jButton1 = new JButton("clear");
-        JButton jButton2 = new JButton("send");
+        header.setPreferredSize(new Dimension(200, 50));
+        add(header, BorderLayout.NORTH);
+        add(s2, BorderLayout.CENTER);
+        add(s3, BorderLayout.EAST);
+        add(s4, BorderLayout.WEST);
+        clear = new JButton("Clear");
+        send = new JButton("Send");
         ButtonListener listener = new ButtonListener();
-        jButton1.addActionListener(listener);
-        jButton2.addActionListener(listener);
-        JPanel bot = new JPanel(true);
-        bot.add(jButton1);
-        bot.add(jButton2);
-        add (bot, BorderLayout.SOUTH);
-        ButtonGroup groupBodyStructure = new ButtonGroup();
-        groupBodyStructure.add (jButton1);
-        groupBodyStructure.add (jButton2);
+        clear.addActionListener(listener);
+        send.addActionListener(listener);
+        send.setVisible(false);
+        buttonPanel = new JPanel();
+        buttonPanel.setPreferredSize(new Dimension(200, 40));
+        buttonPanel.setBackground(Color.cyan);
+        buttonPanel.add(clear);
+        buttonPanel.add(send);
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        setPreferredSize(new Dimension(200, 80));
+        setBackground(Color.cyan);
+
         s4.inputActualWeight.addFocusListener(new FocusListener() {
-            @Override public void focusGained(FocusEvent e) {}
+            @Override
+            public void focusGained(FocusEvent e) {
+            }
+
             @Override
             public void focusLost(FocusEvent e) {
                 String text = s4.inputActualWeight.getText();
-                if(!text.isEmpty()) s2.Weight = Float.parseFloat (text);
+                if (!text.isEmpty()) s2.Weight = Float.parseFloat(text);
             }
         });
 
     }
 
     private class ButtonListener implements ActionListener {
-         public void actionPerformed(ActionEvent event) {
-             double slimness;
-             if(s2.bodyStructure.equals("small")) slimness = 0.9;
-             else if (s2.bodyStructure.equals("medium")) slimness = 1;
-             else slimness = 1.1;
-             double inputActualWeight = Integer.parseInt(s4.inputActualWeight.getText());
-             double idealWeight = (s3.sendWeightSlider() - 100 + (inputActualWeight / 10)) * 0.9 * slimness;
-             new Answer(s2.Weight/((s3.sendWeightSlider())*(s3.sendWeightSlider())), idealWeight,s2.Weight);
-             s2.groupBodyStructure.clearSelection();
-             s2.groupGender.clearSelection();
-             s3.clearWeightSlider();
-             s4.inputActualWeight.setText("");
-             s4.inputFirstName.setText("");
-             s4.inputLastName.setText("");
-             s4.inputAge.setText("");
-         }
-    }
 
+        public void actionPerformed(ActionEvent event) {
+            if (event.getSource() == send){
+                double slimness = 0,inputAge = 0;
+                if (s2.bodyStructure.equals("small")) slimness = 0.9;
+                else if (s2.bodyStructure.equals("medium")) slimness = 1;
+                else slimness = 1.1;
+                inputAge = Integer.parseInt(s4.inputAge.getText());
+
+                double idealWeight = (s3.sendWeightSlider() - 100 + (inputAge / 10)) * 0.9 * slimness;
+                new Answer(s2.Weight / ((s3.sendWeightSlider()) * (s3.sendWeightSlider())), idealWeight, s2.Weight);
+                send.setVisible(false);
+            }
+            s2.groupBodyStructure.clearSelection();
+            s2.groupGender.clearSelection();
+            s3.clearWeightSlider();
+            s4.inputActualWeight.setText("");
+            s4.inputFirstName.setText("");
+            s4.inputLastName.setText("");
+            s4.inputAge.setText("");
+        }
+    }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.imageIcon.paintIcon(this, g, 0, 0);
-
+        if(s2.bodyStructure != null && !s4.inputAge.getText().isEmpty() && !s4.inputActualWeight.getText().isEmpty()) {
+            send.setVisible(true);
         }
+        repaint();
     }
+}
+
+
+
 
